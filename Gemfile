ec4d44da910f5b13dfaa4e0a1229ed34ec22e622
joaquimluis <joaquim.mrvl@gmail.com>
2022-08-19 15:22:50 +0100

    seed random model, show movie poster on list, add dotenv-rails

--- Gemfile
+++ Gemfile
@@ -57,6 +57,9 @@ gem "simple_form", github: "heartcombo/simple_form"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+
+  gem "faker"
+
 end
 
 group :development do
@@ -72,3 +75,4 @@ end
 
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem 'dotenv-rails', groups: [:development, :test]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+data_serialized = URI.open(url).read
+data = JSON.parse(data_serialized)
+
+puts 'Cleaning records'
+Movie.destroy_all
+
+puts 'Creating Lists'
+lists = []
+rand(5..10).times do
+  lists << List.create!(name: Faker::Emotion.unique.noun)
+end
+
 puts 'Creating movies'
+img_url = 'https://image.tmdb.org/t/p/w200'
+movies = []
+20.times do |index|
+  result = data["results"][index]
+  movies << Movie.create!(title: result["title"], overview: result["overview"], poster_url: img_url + result["poster_path"], rating: result["vote_average"] )
+end
 
+puts 'Creating bookmarks'
+lists.each do |list|
+  rand(5..10).times do
+    movie = movies.sample
+    Bookmark.create!(comment: Faker::Quote.yoda, movie_id: movie.id, list_id: list.id) unless movie.nil?
+    movies.empty? ? break : movies.delete(movie)
+  end
+end
+
+# Faker::Quote.yoda
+# Faker::Creature::Animal.name:
 # db/seeds.rb
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-
 puts 'Finished'
